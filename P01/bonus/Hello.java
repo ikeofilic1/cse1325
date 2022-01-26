5f94e038f79ac970990f55b238f76c31ef33aec5
Ike Ofili <ikeofilic1@gmail.com>
2022-01-25 21:02:09 -0600

    Cleanup

--- P01/bonus/Hello.java
+++ P01/bonus/Hello.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Hello {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		System.out.print("What is your name? ");
 		String name = sc.nextLine();
 		System.out.println("Hello, " + name + "!");
