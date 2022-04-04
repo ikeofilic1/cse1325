9e183ec07292173955fde6e00a8ed50a2bf83f63
ikeofilic1 <ikeofilic1@gmail.com>
2022-04-04 15:03:32 -0500

    bonus1

--- P08/bonus/hello_all.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::string name;
-    std::cout << "What's your name? ";
-    getline(std::cin, name);
-    std::cout << "Hello, " << name << "!" << std::endl;
-    return 0;
-} 
