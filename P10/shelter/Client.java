321f4384de8ca597e556ae1d7bdca437e4455366
ikeofilic1 <62239576+ikeofilic1@users.noreply.github.com>
2022-04-18 20:02:13 -0500

    Update Client.java

--- P10/shelter/Client.java
+++ P10/shelter/Client.java
@@ -1,5 +1,30 @@
 package shelter;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
 public class Client {
-    
+	private String name;
+	private String phone;
+
+	public Client(String name, String phone){
+		this.name = name;
+		this.phone = phone;
+	}
+
+	public Client(BufferedReader br) throws IOException{
+		name = br.readLine();
+		phone = br.readLine();
+    }
+
+    public void save(BufferedWriter bw) throws IOException {  
+    	bw.write("" + name + '\n');
+    	bw.write("" + phone + '\n');
+    }
+
+    @Override
+    public String toString(){
+    	return name + " (" + phone + ") ";
+    }
 }
